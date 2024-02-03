22bc242f137cef514aad6b8dca63285768b5d355
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-02-03 23:34:18 +0900

    chore : 쇼핑몰 만들기(header, list페이지)

---
+++ fresh/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  let name = "kim";
+  return (
+    <div>
+      <h4 className="title">시연후레시</h4>
+      <p className="title-sub">by dev {name}</p>
+    </div>
+  );
+}

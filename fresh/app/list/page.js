f819ca12634b79205f2b3dc97ef008c6936f9838
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-02-09 21:35:25 +0900

    0209

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,12 +1,17 @@
 export default function List() {
+  let 상품 = ["Tomatoes", "Pasta", "Cocount"];
+
   return (
     <div>
       <h2>Products</h2>
       <div className="food">
-        <h4>상품명 $40</h4>
+        <h4>{상품[0]} $40</h4>
       </div>
       <div className="food">
-        <h4>상품명 $40</h4>
+        <h4>{상품[1]} $40</h4>
+      </div>
+      <div className="food">
+        <h4>{상품[2]} $40</h4>
       </div>
     </div>
   );

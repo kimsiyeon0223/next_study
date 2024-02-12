f92f399edf1bb3ca3ab62ceb9ecf24c7024e1874
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-02-12 20:19:31 +0900

    이미지 import

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,3 +1,6 @@
+// Image 문법을 쓸려면 사진들을 import해서 가져와 써야함
+import Image from "next/image";
+import to from "/public/food0.png";
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Cocount"];
 
@@ -7,6 +10,8 @@ export default function List() {
       {상품.map((a, i) => {
         return (
           <div className="food" key={i}>
+            {/* 외부 이미지를 쓸 경우 : 링크를 넣고 width와 height를 지정해줘야 함 */}
+            <Image src={to} className="food-img" />
             <h4>{a} $40</h4>
           </div>
         );

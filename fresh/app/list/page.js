984e3c385b0143399ae7f117e99ce15a54a86f98
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-02-12 20:12:03 +0900

    map 적용

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,23 +1,16 @@
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Cocount"];
-  let rr = [2, 3, 4];
-  rr.map((a, i) => {
-    console.log(a);
-    console.log(i);
-  });
 
   return (
     <div>
-      <h2>Products</h2>
-      <div className="food">
-        <h4>{상품[0]} $40</h4>
-      </div>
-      <div className="food">
-        <h4>{상품[1]} $40</h4>
-      </div>
-      <div className="food">
-        <h4>{상품[2]} $40</h4>
-      </div>
+      <h4 className="title">상품목록</h4>
+      {상품.map((a, i) => {
+        return (
+          <div className="food" key={i}>
+            <h4>{a} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }

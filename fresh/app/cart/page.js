00bad56814f5216ea27f02b89b66a69f469c57fa
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-02-14 20:26:16 +0900

    props 문법

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,21 +1,24 @@
-import { age, name } from "./data";
-
 export default function Cart() {
+  let 장바구니 = ["Tomatoes", "Pasta"];
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <CartItem />
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      <CartItem item={장바구니[0]} />
+      <CartItem item={장바구니[1]} />
+      <Banner content="롯데카드" />
+      <Banner content="현대카드" />
     </div>
   );
 }
 
-function CartItem() {
+function Banner(props) {
+  return <h5>{props.content} 결제 행사중</h5>;
+}
+
+function CartItem(props) {
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      <p>{props.item}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
@@ -32,3 +35,6 @@ function CartItem() {
 // - 로딩속도 느림1 ( 자바스크립트 많이 필요함 )
 // - 로딩속도 느림2 (hydration 필요)
 // hydration : html을 유저에게 보낸 후 자바스크립트로 html을 다시 읽고 분석하는 일
+
+// 파라미터 문법 == props 문법
+// : 매번 약간 다른 내용으로 function을 실행하고 싶을 때 사용

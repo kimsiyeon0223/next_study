cf7c6f83561c9261511c2d2a2b8d57d9ce0ba1bf
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-02-20 21:09:56 +0900

    수정

--- forum/app/layout.js
+++ forum/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <div className="navbar">
+        <Link href="/" className="logo">
+          Appleforum
+        </Link>
+        <Link href="/list">List</Link>
+      </div>
     </html>
   );
 }

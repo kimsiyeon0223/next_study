385b64ab2aa78eed3670b73f60aa55889bd76a61
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-02-21 17:13:01 +0900

    props 편리하게 사용하기

--- forum/app/list/ListItem.js
+++ forum/app/list/ListItem.js
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 
-export default function ListItem(props) {
+export default function ListItem({ result }) {
   // //useEffect는 html부터 유저에게 보여주고 실행됨 -> 검색노출이 어려울수있음
   // useEffect(() => {
   //     서버에 부탁해서 DB게시물 가져옴
@@ -10,15 +10,13 @@ export default function ListItem(props) {
   // }, [])
 
   {
-    props.result.map((a, i) => {
+    result.map((a, i) => {
       // return()+중괄호 생략가능
       return (
         <div className="list-item" key={i}>
           {/* link에도 prefetch기능이 내장되어 있음 */}
-          <Link href={"/detail/" + props.result[i]._id}>
-            {props.result[i].title}
-          </Link>
-          <Link href={"/edit/" + props.result[i]._id}>수정</Link>
+          <Link href={"/detail/" + result[i]._id}>{result[i].title}</Link>
+          <Link href={"/edit/" + result[i]._id}>수정</Link>
           <p>1월 1일</p>
         </div>
       );

a5113e29392dfaf4e2018a1ccb72877dc1b17a0c
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-02-21 21:30:34 +0900

    게시물 삭제 기능

--- forum/app/list/ListItem.js
+++ forum/app/list/ListItem.js
@@ -1,25 +1,38 @@
 "use client";
 import Link from "next/link";
-// import { useEffect } from "react";
 
 export default function ListItem({ result }) {
-  // //useEffect는 html부터 유저에게 보여주고 실행됨 -> 검색노출이 어려울수있음
-  // useEffect(() => {
-  //     서버에 부탁해서 DB게시물 가져옴
-  //     result = DB 게시물
-  // }, [])
-
-  {
-    result.map((a, i) => {
-      // return()+중괄호 생략가능
-      return (
+  return (
+    <div>
+      {result.map((a, i) => (
         <div className="list-item" key={i}>
-          {/* link에도 prefetch기능이 내장되어 있음 */}
           <Link href={"/detail/" + result[i]._id}>{result[i].title}</Link>
-          <Link href={"/edit/" + result[i]._id}>수정</Link>
-          <p>1월 1일</p>
+          <Link href={"/edit/" + result[i]._id} className="list-btn">
+            ✏️
+          </Link>
+          <span
+            onClick={() => {
+              fetch("/api/post/delete", {
+                method: "DELETE",
+                body: result[i]._id,
+              })
+                .then((r) => r.json())
+                .then((result) => {
+                  //성공시
+                });
+            }}
+          >
+            🗑️
+          </span>
+          <span>1월 1일</span>
         </div>
-      );
-    });
-  }
+      ))}
+    </div>
+  );
 }
+
+// //useEffect는 html부터 유저에게 보여주고 실행됨 -> 검색노출이 어려울수있음
+// useEffect(() => {
+//     서버에 부탁해서 DB게시물 가져옴
+//     result = DB 게시물
+// }, [])

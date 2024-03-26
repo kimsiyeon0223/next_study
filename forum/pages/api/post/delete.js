63f372826831aacbd0a28771f6e62915849ee37b
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-03-26 23:17:53 +0900

    코드 수정

--- forum/pages/api/post/delete.js
+++ forum/pages/api/post/delete.js
@@ -1,9 +1,13 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(요청, 응답) {
   if (요청.method == "DELETE") {
     console.log(요청.body);
+    let session = await getServerSession(요청, 응답, authOptions);
+
     const db = (await connectDB).db("forum");
     let result = await db
       .collection("post")

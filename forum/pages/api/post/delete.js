eb2eea1b0973f266cb1185b01ad0e3f4f26ba7c3
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-03-31 22:56:07 +0900

    회원 게시물 삭제 기능

--- forum/pages/api/post/delete.js
+++ forum/pages/api/post/delete.js
@@ -14,5 +14,7 @@ export default async function handler(요청, 응답) {
       .deleteOne({ _id: new ObjectId(요청.body) });
     console.log(result);
     응답.status(200).json("삭제완료");
+  } else {
+    return 응답.status(500).json("현재유저와 작성자 불일치");
   }
 }
